b68f0912c10da7f995facf6e023f8fbf6270dd5e
Teemu Vaalasmaa <flipsuk@gmail.com>
2024-11-28 15:15:12 +0200

    updated to nextjs 15

--- app/components/CurrentTime.jsx
+++ app/components/CurrentTime.jsx
@@ -2,11 +2,19 @@
 import { useState, useEffect } from 'react';
 
 export default function CurrentTime() {
-    const [time, setTime] = useState(new Date().toLocaleTimeString('fi-FI'));
+    const [time, setTime] = useState(new Date().toLocaleTimeString('fi-FI', {
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false
+    }));
 
     useEffect(() => {
         const timerId = setInterval(() => {
-            setTime(new Date().toLocaleTimeString('fi-FI'));
+            setTime(new Date().toLocaleTimeString('fi-FI', {
+                hour: '2-digit', 
+                minute: '2-digit',
+                hour12: false
+            }));
         }, 1000);
         return () => clearInterval(timerId);
     }, []);
