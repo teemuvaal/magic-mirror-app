876bc1f101c44cceb78487ae91bd28dbbaf07470
Teemu Vaalasmaa <flipsuk@gmail.com>
2024-12-12 15:54:09 +0200

    adjust layout to fit content better on screen

--- app/page.jsx
+++ app/page.jsx
@@ -8,18 +8,18 @@ import settings from '../config.js'
 export default function StopPage() {
 
   return (
-    <div className="ml-5 mt-5">
+    <div className="ml-5 mt-2">
       <div className="flex justify-between flex-row-reverse mr-10 mb-2">
       <CurrentTime />
       <CurrentDayWeather city={settings.location} />
       <CurrentDate />
       </div>
-      <div className='my-5'>
+      <div className='my-2'>
       {/* Telegram messages */}
       <MessageBox />
       </div>
       {/* Stops are defined in magicmirror.config.js */}
-      <div className='flex flex-col gap-5'>
+      <div className='flex flex-col gap-2'>
         {settings.stops.map((stop, index) => (
           <StopInfo key={index} id={stop.id} type={stop.type} />
         ))}
